3bc481a0da107a731e9d1fb88a064aa59e80c6da
Sergii Gutsuliak <wheelyhog@gmail.com>
2022-10-24 22:31:11 +0200

    add banner-slider

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+  centeredSlides: true,
+  autoplay: {
+    delay:10000,
+    disableOnInteraction: false,
+    speed: 5000,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+
+  },
+
+
+});
